494c867e1a6f8d253687d9c960da48c554372e7f
Steven Katz <steven.katz@verizonmedia.com>
2020-03-17 14:17:13 -0400

    Refactor game initialization, added parameter validation to start

--- src/footballsquares/core.clj
+++ src/footballsquares/core.clj
@@ -5,7 +5,7 @@
 (load "player")
 (load "printing")
 
-(defn init-game [[home away & players]]
+(defn- init-game [[home away & players]]
   {home   :home
    away   :away
    :teams {:home {:name  home
@@ -14,15 +14,18 @@
                   :score '()}}
    :board (squares players)})
 
+(defn- play-game [c g]
+  (let [cmd (s/split c #" ")
+        new-game (condp = (first cmd)
+                   "score" (do-score (rest cmd) g)
+                   "unscore" (un-score (first (rest cmd)) g)
+                   "quit" (System/exit 0)
+                   g)]
+    (print-squares new-game)
+    (recur (read-line) new-game)))
+
 (defn -main [& args]
-    (let [game (init-game args)
-          play-game (fn [c g]
-                        (let [cmd (s/split c #" ")
-                              new-game (condp = (first cmd)
-                                         "score" (do-score (rest cmd) g)
-                                         "unscore" (un-score (first (rest cmd)) g)
-                                         "quit" (System/exit 0)
-                                         g)]
-                          (print-squares new-game)
-                          (recur (read-line) new-game)))]
-        (play-game "print" game)))
+  (when (< (count args) 3)
+    (println "You must supply: home-team-name visiting-team-name players*")
+    (System/exit -1))
+  (play-game "print" (init-game args)))
